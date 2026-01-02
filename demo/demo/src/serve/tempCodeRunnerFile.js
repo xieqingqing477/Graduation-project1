2faf2ca1ef3ca528dc0d163c7d35d13318b2e06f
xieqingqing477 <15980438351@163.com>
2026-01-02 09:01:00 +0800

    Delete demo directory

--- demo/demo/src/serve/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-// 生成验证码
-// app.get("/captcha", (req, res) => {
-//     const captcha = svgCaptcha.create();
-//     // 返回验证码文本和svg图片数据
-//     res.send({
-//         text: captcha.text,
-//         data: captcha.data,
-//     });
-// });
